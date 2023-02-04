51df7b5ad911bd990a5e3ce6de019ef2fb572c24
rakib <rakibul241221@gmail.com>
2023-02-05 01:12:19 +0600

    added redux practice project

---
+++ src/components/ProductCard.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { BiListPlus } from "react-icons/bi";
+import { useDispatch } from "react-redux";
+import { useLocation } from "react-router-dom";
+import { addToCart, removeFromCart } from "../redux/actionCreators/productActions";
+import { ADD_TO_CART } from "../redux/actionTypes/actionTypes";
+import { RiDeleteBin6Fill } from 'react-icons/ri'
+
+const ProductCard = ({ product }) => {
+  const dispatch = useDispatch()
+  const { pathname } = useLocation()
+  return (
+    <div
+      className='shadow-lg rounded-3xl border relative  p-3 flex flex-col text-indigo-900'
+      key={product._id}
+    >
+      {pathname.includes("cart") &&
+        <div className="bg-indigo-500 absolute top-2 right-2 text-white rounded-full h-8 w-8">
+          <p className="text-center">{product.quantity}</p>
+        </div>}
+      <div className='h-52 w-52 mx-auto'>
+        <img src={product.image} alt={product.model} />
+      </div>
+      <h1 className='font-bold text-center'>{product.model}</h1>
+      <p className='text-center font-semibold mb-3'>Rating: {product.rating}</p>
+      <div className=' flex-1'>
+        <ul className='space-y-2'>
+          {product.keyFeature.map((feature) => {
+            return <li
+              key={feature}
+              className='text-sm '>{feature}</li>;
+          })}
+        </ul>
+      </div>
+      <div className='flex gap-2 mt-5'>
+        {pathname.includes("cart") && <button
+          onClick={() => dispatch(removeFromCart(product))}
+          className='bg-red-500 rounded-full py-1 flex-1 flex px-4 justify-between items-center text-white text-bold'>
+          <RiDeleteBin6Fill className="text-lg" />
+          <p>Remove</p>
+        </button>}
+        {!pathname.includes("cart") &&
+          <button
+            onClick={() => dispatch(addToCart(product))}
+            className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'>
+            Add to cart
+          </button>}
+
+        {
+          !pathname.includes("cart") &&
+          <button
+            title='Add to wishlist'
+            className='bg-indigo-500  py-1 px-2 rounded-full'
+          >
+            <BiListPlus className='text-white' />
+          </button>}
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,44 @@
+import { ADD_TO_CART, REMOVE_FROM_CART } from "../actionTypes/actionTypes";
+
+const initialState = {
+    cart: []
+}
+const productReducer = (state = initialState, action) => {
+    const selectedProduct = state.cart.find((product) => product.id === action.payload.id)
+    console.log(selectedProduct)
+
+    switch (action.type) {
+        case ADD_TO_CART:
+            if (selectedProduct) {
+                const newcart = state.cart.filter(product => product.id !== selectedProduct.id);
+                selectedProduct.quantity = selectedProduct.quantity + 1;
+                return {
+                    ...state,
+                    cart: [...newcart, selectedProduct]
+                }
+            }
+            return {
+                ...state,
+                cart: [...state.cart, { ...action.payload, quantity: 1 }]
+            };
+
+        case REMOVE_FROM_CART:
+            if (selectedProduct.quantity > 1) {
+                const newcart = state.cart.filter(product => product.id !== selectedProduct.id);
+                selectedProduct.quantity = selectedProduct.quantity - 1;
+                return {
+                    ...state,
+                    cart: [...newcart, selectedProduct]
+                }
+            }
+            return {
+                ...state,
+                cart: state.cart.filter(product => product.id !== action.payload.id)
+            };
+
+        default:
+            return state;
+    }
+}
+
+export default productReducer;
